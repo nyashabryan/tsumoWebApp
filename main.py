5be2e3bf4ed3d06197af49fb49ed61ac0e2e25d4
Nyasha <nyashabryan23@gmail.com>
2018-07-25 01:43:44 +0100

    Changed to gitignore

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 
+# Nyasha 
+
 import os.path
 
 import classes
 import db
 
-print (os.getcwd())
+print(os.getcwd())
 STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '/static/')
 
 app = Flask(__name__)
@@ -48,29 +50,32 @@ def get_tsumo(tsumo_no):
     return render_template('tsumo.html', page = page)
 
 @app.route("/new")
+
 def new_tsumo():
 
     page = classes.Page("New Tsumo")
     page.tsumo = db.newest_tsumo()
 
-    return render_template('newest.html', page = page)
+    return render_template('newest.html', page= page)
 
 
 @app.route("/contribute")
+
 def contribute_tsumo():
 
     page = classes.Page("Contribute")
     
-    return render_template("contribute.html")
+    return render_template("contribute.html") 
 
 
 @app.route("/downloads")
+
 def downloads():
     page = classes.Page("Downloads")
-    return render_template("downloads.html", page = page)
+    return render_template("downloads.html", page=page)
 
 @app.route("/about")
+
 def about():
     page = classes.Page("About")
-    
-    return render_template("about.html", page = page)
+    return render_template("about.html", page=page)
